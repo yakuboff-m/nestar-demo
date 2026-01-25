0a9a6612ef2c10a41f42d9fffe52ca7ce412e427
yakuboff-m <m.yakuboff.15@gmail.com>
2026-01-25 19:09:11 +0900

    feat: meta tags integrations on document private file

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,20 @@ export default function Document() {
   console.log("Document COMPONENT - PAGES ROUTER");
   return (
     <Html lang="en">
-      <Head />
+      <Head> 
+        <meta charSet="UTF-8" />
+        <meta name="title" content="Nestar demo" />
+        <meta name="robots" content="index,follow" />
+        <link rel="icon" type="image/png" href="/img/logo/favicon.svg" />
+
+        {/* SEO (Search engine optimization) */}
+        <meta name="keyword" content={"nestar, nestar.uz, devex mern, mern nestjs fullstack"}/>
+        <meta name="description" content={
+          "Buy and sell properties anywhere anytime in South Korea. |" +
+          "Покупайте и продавайте недвижимость в любой точке Южной Кореи в любое время. |" +
+          "대한민국 어디에서든 언제든지 부동산을 사고 팔 수 있습니다."
+        }/>
+      </Head>
       <body>
         <Main />
         <NextScript />
